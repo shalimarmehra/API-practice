7ab75a70bef176bcac86eda75cc4b9b7a72acb99
shalimarmehra <shalimarmehraa@gmail.com>
2024-09-16 21:46:28 +0530

    first

---
+++ src/index.mjs
@@ -0,0 +1,29 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (request, response) =>{
+    response.status(201).send({msg: "Hello World!"})
+})
+
+app.get("/api/users", (request, response) => {
+    response.status(201).send([
+        {id: "1", username: "Shalimar", displayname: "sh"},
+        {id: "2", username: "jack", displayname: "ja"},
+        {id: "3", username: "ricky", displayname: "rick"}
+    ])
+})
+
+app.get("/api/products", (request, response) => {
+    response.send([{id: 123, name: 'chicken brest', price: 12.45}])
+})
+
+app.listen(PORT, () => {
+    console.log(`Running on PORT ${PORT}`)
+})
+
+// localhost:3000
+// localhost:3000/users
+// localhost:3000/products
